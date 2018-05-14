f448d2e1a715a75955d18037d144066bdce01229
Scott Weitze <sweitze@stevense.edu>
2018-05-14 14:57:20 -0400

    Queue Capacity Change

--- PathTrain.cc
+++ PathTrain.cc
@@ -21,89 +21,89 @@
 using namespace omnetpp;
 
 
-class Source: public cSimpleModule {
-private:
-    int jobsLeft;
-    int trainsInSystem;
-    int maxTrainsInSystem;
-
-public:
-    Source();
-    virtual ~Source();
-
-protected:
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
-};
-
-Define_Module(Source);
-
-void Source::initialize() {
-
-    // Generate and send Path Train
-
-    //maxTrainsInSystem is the number of path trains between the
-    //two stations
-    maxTrainsInSystem = par("maxTrains"); //read the value from the
-                                   //ARQ.ned declared parameter
-
-
-
-
-}
-
-void Source::handleMessage(cMessage *msg) {
-
-}
-
-
-class Destination: public cSimpleModule {
-protected:
-    cStdDev delayStats;
-    cDoubleHistogram histogram;
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
-    virtual void finish() override;
-};
-
-Define_Module(Destination);
-
-void Destination::initialize() {
-    delayStats.setName("Delay");
-    //histogram.setNumCells(20);
-    //histogram.setRangeAuto(50, 1.5);
-    histogram.setRange(0, 1);
-    histogram.setNumCells(10);
-
-}
-
-void Destination::handleMessage(cMessage *msg) {
-    cPacket* pkt = check_and_cast<cPacket *>(msg);
-    if (pkt->hasBitError()) {
-        EV << "Packet received in error";
-        pkt->setKind(1); //kind =1 means NACK
-    } else {
-        EV << "Sending back same message as ACK.\n";
-        pkt->setKind(0); // kind=0 - means ACK - we need to collect statistics
-        simtime_t delay = simTime() - pkt->getCreationTime();
-        EV << "delay =  ";
-        EV << delay;
-        delayStats.collect(delay);
-        histogram.collect(delay);
-    }
-    send(pkt, "out");
-}
-
-void Destination::finish() {
-    recordScalar("mean delay", delayStats.getMean());
-    recordScalar("max delay", delayStats.getMax());
-    recordScalar("std deviation", delayStats.getStddev());
-    recordScalar("variance", delayStats.getVariance());
-    histogram.record();
-    //test if the selected range was appropriate:
-    EV << "histogram number of overflows =";
-    EV << histogram.getOverflowCell();
-}
+//class Source: public cSimpleModule {
+//private:
+//    int jobsLeft;
+//    int trainsInSystem;
+//    int maxTrainsInSystem;
+//
+//public:
+//    Source();
+//    virtual ~Source();
+//
+//protected:
+//    virtual void initialize() override;
+//    virtual void handleMessage(cMessage *msg) override;
+//};
+//
+//Define_Module(Source);
+//
+//void Source::initialize() {
+//
+//    // Generate and send Path Train
+//
+//    //maxTrainsInSystem is the number of path trains between the
+//    //two stations
+//    maxTrainsInSystem = par("maxTrains"); //read the value from the
+//                                   //ARQ.ned declared parameter
+//
+//
+//
+//
+//}
+//
+//void Source::handleMessage(cMessage *msg) {
+//
+//}
+//
+//
+//class Destination: public cSimpleModule {
+//protected:
+//    cStdDev delayStats;
+//    cDoubleHistogram histogram;
+//    virtual void initialize() override;
+//    virtual void handleMessage(cMessage *msg) override;
+//    virtual void finish() override;
+//};
+//
+//Define_Module(Destination);
+//
+//void Destination::initialize() {
+//    delayStats.setName("Delay");
+//    //histogram.setNumCells(20);
+//    //histogram.setRangeAuto(50, 1.5);
+//    histogram.setRange(0, 1);
+//    histogram.setNumCells(10);
+//
+//}
+//
+//void Destination::handleMessage(cMessage *msg) {
+//    cPacket* pkt = check_and_cast<cPacket *>(msg);
+//    if (pkt->hasBitError()) {
+//        EV << "Packet received in error";
+//        pkt->setKind(1); //kind =1 means NACK
+//    } else {
+//        EV << "Sending back same message as ACK.\n";
+//        pkt->setKind(0); // kind=0 - means ACK - we need to collect statistics
+//        simtime_t delay = simTime() - pkt->getCreationTime();
+//        EV << "delay =  ";
+//        EV << delay;
+//        delayStats.collect(delay);
+//        histogram.collect(delay);
+//    }
+//    send(pkt, "out");
+//}
+//
+//void Destination::finish() {
+//    recordScalar("mean delay", delayStats.getMean());
+//    recordScalar("max delay", delayStats.getMax());
+//    recordScalar("std deviation", delayStats.getStddev());
+//    recordScalar("variance", delayStats.getVariance());
+//    histogram.record();
+//    //test if the selected range was appropriate:
+//    EV << "histogram number of overflows =";
+//    EV << histogram.getOverflowCell();
+//}
 
 
 
